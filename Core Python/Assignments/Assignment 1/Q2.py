652286c10399f12681301576d0996dda4560ae10
Saee Mule <mulesaee14@gmail.com>
2026-07-13 00:15:10 +0530

    Delete Core Python/Assignments/Assignment 1 directory

--- Core Python/Assignments/Assignment 1/Q2.py
+++
@@ -1,9 +0,0 @@
-#Take input from length & breadth
-length = int(input('Enter length:'))
-breadth = int(input('Enter breadth:'))
-
-#Perform area 
-area = length*breadth
-
-#Perfrom result
-print(f'Area of {length} & {breadth} is {area}.')
