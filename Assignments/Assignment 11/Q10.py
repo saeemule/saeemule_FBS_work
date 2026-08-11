70d05f7c961ffc19717016a877e26425ea4119ad
Saee Mule <mulesaee14@gmail.com>
2026-08-11 22:04:03 +0530

    Add files via upload

---
+++ Assignments/Assignment 11/Q10.py
@@ -0,0 +1,3 @@
+numbers = [10, 15, 22, 33, 40, 51, 68]
+odd_list = [num for num in numbers if num % 2 != 0]
+print("List after removing even numbers =", odd_list)
---
+++ Assignments/Assignment 11/Q2.py
@@ -0,0 +1,5 @@
+list1 = [10, 5, 30, 15]
+list2 = [8, 25, 3, 40]
+merged_list = list1 + list2
+merged_list.sort()
+print("Merged and sorted list =", merged_list)
