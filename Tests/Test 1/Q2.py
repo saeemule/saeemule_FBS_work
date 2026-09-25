6b32bc96a0ea480cbf17250045e9b96051aba68e
Saee Mule <mulesaee14@gmail.com>
2026-09-25 23:12:42 +0530

    Delete Tests directory

--- Tests/Test 1/Q2.py
+++
@@ -1,7 +0,0 @@
-principal = float(input("Enter Principal amount: "))
-rate = float(input("Enter Rate of interest: "))
-time = float(input("Enter Time (in years): "))
-
-simple_interest = (principal * rate * time) / 100
-
-print(f"Simple Interest = {simple_interest:.2f}")
--- Tests/Test 3/Q4.py
+++
@@ -1,7 +0,0 @@
-rows = 5
-
-for i in range(rows):
-    if i % 2 == 0:
-        print("10101")
-    else:
-        print("01010")
